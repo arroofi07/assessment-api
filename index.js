e96f351625c31da0bb8155a06610312c31ac5991
arovi <arrooficr7@gmail.com>
2025-04-10 02:44:03 +0700

    comments

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ const express = require("express");
 const home = require("./routes/home");
 const mahasiswa = require("./routes/Mahasiswa");
 const story = require("./routes/Story");
+const comments = require("./routes/Comments");
 
 // Middlewares
 const app = express();
@@ -12,6 +13,7 @@ app.use(express.json());
 app.use("/home", home);
 app.use("/mahasiswa", mahasiswa);
 app.use("/story", story);
+app.use("/comments", comments);
 
 // connection
 const port = process.env.PORT || 9001;
